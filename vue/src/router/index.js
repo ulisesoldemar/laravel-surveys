1245385953d414d38a40a1e997e7143972808b75
Ulises Ortega <ulises.ortega2937@alumnos.udg.mx>
2022-04-22 00:57:11 -0500

    Añadidas rutas para las surveys

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -6,6 +6,7 @@ import Dashboard from '../views/Dashboard.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 import Surveys from '../views/Surveys.vue';
+import SurveyView from '../views/SurveyView.vue';
 
 const routes = [
     {
@@ -16,6 +17,8 @@ const routes = [
         children: [
             { path: '/dashboard', name: 'Dashboard', component: Dashboard },
             { path: '/surveys', name: 'Surveys', component: Surveys },
+            { path: '/surveys/create', name: 'SurveyCreate', component: SurveyView },
+            { path: '/surveys/:id', name: 'SurveyView', component: SurveyView },
         ]
     },
     {
