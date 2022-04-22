79a2de8e17883eff2afe775954ff4bd91de98094
Ulises Ortega <ulises.ortega2937@alumnos.udg.mx>
2022-04-22 00:58:30 -0500

    Generadas vistas para las encuestas

---
+++ vue/src/views/SurveyView.vue
@@ -0,0 +1,7 @@
+<template>
+<PageComponent title="View or create a survey"></PageComponent>
+</template>
+
+<script setup>
+import PageComponent from "../components/PageComponent.vue";
+</script>
--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -1,8 +1,28 @@
 <template>
-  <PageComponent title="Surveys"> Contenido </PageComponent>
+  <PageComponent>
+    <template v-slot:header>
+      <div class="flex justify-between items-center">
+        <h1 class="text-3xl font-bold text-gray-900">Surveys</h1>
+        <router-link
+          :to="{ name: 'SurveyCreate' }"
+          class="
+            py-2
+            px-3
+            text-white
+            bg-emerald-500
+            rounded-md
+            hover:bg-emerald-600
+          "
+        >
+          <PlusIcon class="h-4 w-4 -mt-1 inline-block" />
+          New survey
+        </router-link>
+      </div>
+    </template>
+  </PageComponent>
 </template>
 
 <script setup>
+import { PlusIcon } from "@heroicons/vue/solid";
 import PageComponent from "../components/PageComponent.vue";
-
 </script>
