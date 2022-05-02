70809dcef92175413b8d629764498987557fabd0
Ulises Ortega <ulises.ortega2937@alumnos.udg.mx>
2022-05-02 09:57:56 -0500

    Redirección al login en caso de status 401

--- vue/src/lib/axios.js
+++ vue/src/lib/axios.js
@@ -1,4 +1,6 @@
 import Axios from 'axios'
+import { useUsers } from '../stores/user'
+import router from '../router'
 
 const axios = Axios.create({
     baseURL: import.meta.env.VITE_PUBLIC_BACKEND_URL,
@@ -7,5 +9,24 @@ const axios = Axios.create({
     },
     withCredentials: true,
 })
+
+axios.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response.status === 401) {
+            const store = useUsers()
+            store.$reset()
+            if (!store.userLogin.remember) {
+                store.userLogin = {}
+            }
+            store.userData = {}
+            store.authStatus = []
+            router.push({ name: 'login' })
+        } else if (error.response.status === 404) {
+            router.push({ name: 'page-not-found' })
+        }
+        throw error;
+    })
+
 
 export default axios
