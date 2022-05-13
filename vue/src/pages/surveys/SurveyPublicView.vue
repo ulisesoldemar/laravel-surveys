4cfea191b96183beaaaf4009562a657e6fe49e31
Ulises Ortega <ulises.ortega2937@alumnos.udg.mx>
2022-05-13 01:16:06 -0500

    Se muestran errores de validación en el formulario

--- vue/src/pages/surveys/SurveyPublicView.vue
+++ vue/src/pages/surveys/SurveyPublicView.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="py-5 px-8">
-    <div
-      v-if="loading"
-      class="flex justify-center"
-    >
-      <div
-        class="
+    <div v-if="loading" class="flex justify-center">
+      <div class="
           spinner-border
           animate-spin
           inline-block
@@ -13,65 +9,37 @@
           h-12
           border-4
           rounded-full
-          "
-        role="status"
-      ></div>
+          " role="status"></div>
     </div>
-    <form
-      @submit.prevent="submitSurvey"
-      v-else
-      class="container mx-auto"
-    >
+    <form @submit.prevent="submitSurvey" v-else class="container mx-auto">
       <div class="grid grid-cols-6 items-center">
         <div class="mr-4">
-          <img
-            :src="survey.image_url"
-            alt=""
-          />
+          <img :src="survey.image_url" alt="" />
         </div>
         <div class="col-span-5">
           <h1 class="text-3xl mb-3">{{ survey.title }}</h1>
-          <p
-            class="text-gray-500 text-sm"
-            v-html="survey.description"
-          ></p>
+          <p class="text-gray-500 text-sm" v-html="survey.description"></p>
         </div>
       </div>
-      <div
-        v-if="surveyFinished"
-        class="px-6 py-8 bg-emerald-400 text-white w-[600px] mx-auto"
-      >
+      <BreezeValidationErrors class="mb-4" :errors="errors" />
+      <div v-if="surveyFinished" class="px-6 py-8 bg-emerald-400 text-white w-[600px] mx-auto">
         <div class="text-xl mb-3 font-semibold">
           Thank you for participating in this survey.
         </div>
-        <button
-          @click="submitAnotherResponse"
-          type="button"
-          class="inline-flex justify-center px-4 py-2 border border-transparent shadow-sm
+        <button @click="submitAnotherResponse" type="button" class="inline-flex justify-center px-4 py-2 border border-transparent shadow-sm
                 text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700
-                focus:outline-none focus:ring-2 focus:ring-indigo-500"
-        >
+                focus:outline-none focus:ring-2 focus:ring-indigo-500">
           Submit another response
         </button>
       </div>
       <div v-else>
         <hr class="my-3" />
-        <div
-          v-for="(question, i) of survey.questions"
-          :key="question.id"
-        >
-          <QuestionViewer
-            v-model="answers[question.id]"
-            :question="question"
-            :index="i"
-          />
+        <div v-for="(question, i) of survey.questions" :key="question.id">
+          <QuestionViewer v-model="answers[question.id]" :question="question" :index="i" />
         </div>
-        <button
-          type="submit"
-          class="inline-flex justify-center px-4 py-2 border border-transparent shadow-sm
+        <button type="submit" class="inline-flex justify-center px-4 py-2 border border-transparent shadow-sm
                 text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700
-                focus:outline-none focus:ring-2 focus:ring-indigo-500"
-        >
+                focus:outline-none focus:ring-2 focus:ring-indigo-500">
           Submit
         </button>
       </div>
@@ -81,6 +49,7 @@
 
 <script setup>
 import QuestionViewer from '@/components/viewer/QuestionViewer.vue'
+import BreezeValidationErrors from '@/components/ValidationErrors.vue'
 
 import { computed, ref } from 'vue'
 import { useRoute } from 'vue-router'
@@ -95,6 +64,9 @@ const survey = computed(() => store.currentSurvey.data)
 const surveyFinished = ref(false)
 
 const answers = ref({})
+const setErrors = ref()
+
+const errors = computed(() => setErrors.value)
 
 store.getSurveyBySlug(route.params.slug)
 
@@ -104,12 +76,19 @@ function submitSurvey() {
     .saveSurveyAnswer({
       surveyId: survey.value.id,
       answers: answers.value,
-
     })
     .then((response) => {
       if (response.status === 201) {
         surveyFinished.value = true
       }
+    })
+    .catch((err) => {
+      if (err.response.status !== 422) throw err
+
+      setErrors.value = Object.values(
+        err.response.data.errors,
+      ).flat()
+      loading.value = false
     })
 }
 
@@ -119,5 +98,3 @@ function submitAnotherResponse() {
 }
 
 </script>
-
-<style></style>
